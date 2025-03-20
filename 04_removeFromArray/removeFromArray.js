e48a395de682f426801bda3306ec8974640e3cb6
DigitalMoteur <digitalmoteur@outlook.com>
2025-03-19 21:07:09 -0700

    Remove from array solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...elementsToRemove) {
+    let result = [];
+    for (const element in originalArray) {
+        const value = originalArray[element];
+        if (elementsToRemove.includes(value))
+            continue;
+        result.push(value);
+    }
+    return result;
 };
 
 // Do not edit below this line
