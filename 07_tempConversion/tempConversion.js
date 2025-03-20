0e345efeb6a77cdf516b9af931b26ec42b681914
DigitalMoteur <digitalmoteur@outlook.com>
2025-03-19 21:30:10 -0700

    Temp conversion solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+function finalizeResult(result) {
+  if (result % 1 != 0)
+    return parseFloat(result.toFixed(1));
+  else
+    return result;
+}
+
+const convertToCelsius = function(fahrenheit) {
+  const result = (fahrenheit - 32) / (9/5);
+  return finalizeResult(result);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const result = 32 + (celsius * (9/5));
+  return finalizeResult(result);
 };
 
 // Do not edit below this line
