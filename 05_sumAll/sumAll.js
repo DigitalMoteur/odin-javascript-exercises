0b6000eddf790a643926656ec90be9345f48eb4c
DigitalMoteur <digitalmoteur@outlook.com>
2025-03-19 21:15:37 -0700

    Sum all solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    if (!Number.isInteger(first) || !Number.isInteger(second)
+        || first < 0 || second < 0)
+    {
+        return "ERROR";
+    }
+    if (second < first) {
+        const temp = second;
+        second = first;
+        first = temp;
+    }
+    let sum = 0;
+    for (let idx = first; idx <= second; ++idx) {
+        sum += idx;
+    }
+    return sum;
 };
 
 // Do not edit below this line
