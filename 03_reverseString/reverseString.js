7f4b0b1bbddb6e5956b8b2bcd63dd1415428d739
DigitalMoteur <digitalmoteur@outlook.com>
2025-03-19 19:39:51 -0700

    Reverse string solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let result = '';
+    for (let idx = stringToReverse.length - 1; idx >= 0; --idx) {
+        result += stringToReverse[idx];
+    }
+    return result;
 };
 
 // Do not edit below this line
