9387b2ee9a3ddc202ec2a4876265b6f47a3f579f
DigitalMoteur <digitalmoteur@outlook.com>
2025-03-19 19:35:45 -0700

    First 2 js exercise solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numTimes) {
+    if (numTimes < 0) {
+        return "ERROR";
+    }
+    let result = "";
+    for (let idx = 0; idx < numTimes; ++idx) {
+        result += stringToRepeat;
+    }
+    return result;
 };
 
 // Do not edit below this line
