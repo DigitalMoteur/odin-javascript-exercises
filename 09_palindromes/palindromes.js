64bee88471c60dbcb350acf6f7a7ebe7565eb75f
DigitalMoteur <digitalmoteur@outlook.com>
2025-03-27 22:47:50 -0700

    Palindromes javascript exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let temp = input.split('')
+        .filter((char) => {
+            return char != '!' && char != ' ' && char != '.' && char != ',';
+        })
+        .join('');
+    let reverse = temp.split('').reverse().join('');
+    return reverse.toLowerCase() == temp.toLowerCase();
 };
 
 // Do not edit below this line
