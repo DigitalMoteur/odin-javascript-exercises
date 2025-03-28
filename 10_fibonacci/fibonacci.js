8ea89a23819461a5afa8e34c6bb0015f5f62207f
DigitalMoteur <digitalmoteur@outlook.com>
2025-03-27 22:55:32 -0700

    Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index = parseInt(index);
+    if (index < 0)
+        return "OOPS";
+    const sequence = [0, 1];
+    for (let idx = 2; sequence.length < index + 1; idx++) {
+        sequence[idx] = sequence[idx - 1] + sequence[idx - 2];
+    }
+    console.log(sequence);
+    return sequence[index];
 };
 
 // Do not edit below this line
