b2bcfad09f62f2e81c6e4e1d1feed4edd54eed38
DigitalMoteur <digitalmoteur@outlook.com>
2025-03-27 23:25:19 -0700

    Find the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+function getAge(person) {
+    if (person.yearOfDeath == undefined) {
+        person.yearOfDeath = new Date().getFullYear();
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function(list) {
+    let oldest = undefined;
+    list.map((current) => {
+        if (oldest == undefined)
+            oldest = current;
+        oldest = getAge(current) > getAge(oldest) ? current : oldest
+    });
+    return oldest;
 };
 
 // Do not edit below this line
