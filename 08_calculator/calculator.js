2aa0c7c369f71034359b5b2754c1923384034767
DigitalMoteur <digitalmoteur@outlook.com>
2025-03-27 22:31:35 -0700

    Calculator javascript exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+  if (input == undefined || input.length == 0)
+    return 0;
+	return input.reduce((total, current) => {
+    return total + current;
+  });
 };
 
-const multiply = function() {
-
+const multiply = function(input) {
+  if (input == undefined || input.length == 0)
+    return 0;
+  return input.reduce((total, current) => {
+    return total * current;
+  });
 };
 
-const power = function() {
-	
+const power = function(input, power) {
+  let result = 1;
+  for (let i = 0; i < power; i++)
+  {
+    result *= input;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+	let result = 1;
+  for (let i = 1; i < input + 1; i++)
+  {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
